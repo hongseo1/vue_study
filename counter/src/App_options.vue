a22f562b120077eccf6836d51ea6935d83563bb4
hongseo1 <hongseow109@gmail.com>
2025-07-10 22:23:01 +0900

    Option API와 Composition API의 비교

---
+++ counter/src/App_options.vue
@@ -0,0 +1,35 @@
+<script>
+    export default{
+        data(){
+            return{
+                cnt: 0
+            }
+        },
+        computed:{
+            oddEven(){
+                return(this.cnt%2) ? '홀수' : '짝수';
+            }
+        },
+        methods: {
+            counter(){
+                this.cnt++;
+            }
+        }
+    }
+</script>
+
+<template>
+    <div id="study21" class="study_box">
+        <h3>Composition API</h3>
+        <div>
+            Vue3에서 새롭게 등장한 함수 기반의 API로 컴포넌트 로직을 유연하게 구성하도록 지원하여 코드의 재사용성과 가독성을 높여준다. <br/>
+            기존 Option API와 Composition API의 비교 <br/>
+            - Option API: 지금까지 배운 거, 컴포넌트 로직을 Data, methods, computed, watch 등의 옵션에 담아서 구분함 <br/>
+            - Composition API: Vue3에서 새로 도입된 방식, 함수형 프로그래밍 스타일과 유사 관련 로직을 setup hook에 정의하여 사용, 코드의 재사용성이 높아져서 큰 프로젝트에 적합
+        </div>
+
+        <p>{{cnt}}</p>
+        <p>홀짝 판별: {{oddEven}}</p>
+        <button @click="counter">클릭</button>
+    </div>
+</template>
---
+++ counter/src/App_setup_hook.vue
@@ -0,0 +1,32 @@
+<script>
+    import { computed, ref } from 'vue';
+    export default{
+        setup(){ //Composition API setup hook에 정의 (처음 Composition API도입했을 때 방식(초창기 방식))
+            const cnt = ref(0);
+            const oddEven = computed(() => (cnt.value % 2) ? '홀수' : '짝수');
+            const counter = () => cnt.value++;
+            
+            return{
+                cnt,
+                oddEven,
+                counter,
+            }
+        }
+    }
+</script>
+
+<template>
+    <div id="study21" class="study_box">
+        <h3>Composition API</h3>
+        <div>
+            Vue3에서 새롭게 등장한 함수 기반의 API로 컴포넌트 로직을 유연하게 구성하도록 지원하여 코드의 재사용성과 가독성을 높여준다. <br/>
+            기존 Option API와 Composition API의 비교 <br/>
+            - Option API: 지금까지 배운 거, 컴포넌트 로직을 Data, methods, computed, watch 등의 옵션에 담아서 구분함 <br/>
+            - Composition API: Vue3에서 새로 도입된 방식, 함수형 프로그래밍 스타일과 유사 관련 로직을 setup hook에 정의하여 사용, 코드의 재사용성이 높아져서 큰 프로젝트에 적합
+        </div>
+
+        <p>{{cnt}}</p>
+        <p>홀짝 판별: {{oddEven}}</p>
+        <button @click="counter">클릭</button>
+    </div>
+</template>
