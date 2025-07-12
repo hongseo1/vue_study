77a3f3e6960280dc3df9e97e8548b32acb38f8f3
hongseo1 <hongseow109@gmail.com>
2025-07-12 14:35:09 +0900

    main.js (page35 bootstrap import 정보 업데이트

--- v02/src/main.js
+++ v02/src/main.js
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-import GlobalChild from '@/components/GlobalChild.vue' //14
+import GlobalChild from '@/components/GlobalChild.vue' //page14
+import {BootstrapVue3} from 'bootstrap-vue-3'; //page35
+import 'bootstrap/dist/css/bootstrap.css'; //page35
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css' //page35
 import router from './router'; // router 폴더의 index.js에서 내보낸 라우터 인스턴스 가져오기
 
 const app = createApp(App);
-app.use(router);
+app.use(router); //page35
 app.component("GlobalChild", GlobalChild);
+app.use(BootstrapVue3);
 app.mount('#app');
 
