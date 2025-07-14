7600393f985a4d3995198be086bab554c1104856
hongseo1 <hongseow109@gmail.com>
2025-07-14 15:32:22 +0900

    index.js 페이지 정보 업데이트

--- v02/src/router/index.js
+++ v02/src/router/index.js
@@ -48,6 +48,7 @@ const page_titles = {
   '37': '37. form으로 데이터 입력 받아 서버에 저장하기',
   '38': '38. 이전에 .forEach부분 v-for로 변경하기',
   '39': '39. 서버 데이터 삭제하기',
+  '40': '40. 서버 데이터 수정하기',
   // 필요한 만큼 페이지 제목을 추가
 };
 const routes = [
@@ -57,7 +58,7 @@ const routes = [
     component: home // 이 경로로 접속했을 때 보여줄 Vue 컴포넌트
   },
 ];
-const total_pages = 39; //총 페이지 수
+const total_pages = 40; //총 페이지 수
 
 for(let n=1; n<=total_pages; n++){
   const page_number_string = String(n).padStart(2,'0'); // 01 02 ..로 담음
