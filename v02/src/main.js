133fcf73f7e4f98e96fab89f9fa36d8b22db5478
hongseo1 <hongseow109@gmail.com>
2025-07-16 22:27:44 +0900

    page46 pinia import 및 추가 작업

--- v02/src/main.js
+++ v02/src/main.js
@@ -5,9 +5,13 @@ import {BootstrapVue3} from 'bootstrap-vue-3'; //page35
 import 'bootstrap/dist/css/bootstrap.css'; //page35
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css' //page35
 import router from './router'; // router 폴더의 index.js에서 내보낸 라우터 인스턴스 가져오기 / page35
+import { createPinia } from 'pinia'; //page46
+
+const pinia = createPinia(); //page46
 
 const app = createApp(App);
 app.use(router); //page35
+app.use(pinia); //page46
 app.component("GlobalChild", GlobalChild);
 app.use(BootstrapVue3);
 app.mount('#app');
