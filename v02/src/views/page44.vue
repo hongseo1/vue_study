9e9b79258c2f14a26ec02077fc7e17013937751c
hongseo1 <hongseow109@gmail.com>
2025-07-16 21:37:56 +0900

    Vue Pinia 설명

---
+++ v02/src/views/page44.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="study44" class="study_box">
+        <h3>Pinia (GitHub vue_study/pinia 참고)</h3>
+        <div>
+        애플리케이션의 상태를 관리하기 위한 Vue 공식 라이브러리 Vuex의 후속 버전 <br/>
+        설치: npm install pinia
+        main.js에 pinia import 및 설정
+        HomeView.vue, cnt.js 참고
+        </div>
+    </div>
+</template>
