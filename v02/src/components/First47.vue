68387b0395d182e33428930e431206f360ad0514
hongseo1 <hongseow109@gmail.com>
2025-07-17 16:44:16 +0900

    page47 하위 컴포넌트(Props Drilling / Provide와 Inject를 통한 해결)

---
+++ v02/src/components/First47.vue
@@ -0,0 +1,21 @@
+<!-- page47 -->
+<script setup>
+    import Second47 from './Second47.vue';
+
+    /* const props = defineProps({
+        count: Number
+    }); */
+</script>
+
+<template>
+    <div class="a">
+        <p>First Component</p>
+        <!-- <p>count: {{props.count}}</p>
+        <Second47 :count/> -->
+        <Second47/>
+    </div>
+</template>
+
+<style scoped>
+    .a{border: 3px solid blue; padding: 20px;}
+</style>
---
+++ v02/src/components/Second47.vue
@@ -0,0 +1,23 @@
+<!-- page47 -->
+<script setup>
+    /* 
+    const props = defineProps({
+        count: Number
+    }); 
+    */
+    import { inject } from 'vue'; //inject: 사용할 때
+
+    const count = inject('count');
+</script>
+
+<template>
+    <div>
+        <p>Second Component</p>
+        <!-- count: {{props.count}} -->
+        count: {{count}}
+    </div>
+</template>
+
+<style scoped>
+    div{border: 3px solid green; padding: 20px; margin: 20px;}
+</style>
